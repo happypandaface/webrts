13853ca41c110d80ea171049499de44da3d93fee
Scott Griffy <scottgriffy@gmail.com>
2015-09-18 22:59:37 -0700

    woo

---
+++ py.py
@@ -0,0 +1,25 @@
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
