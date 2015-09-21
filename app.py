00b7aa630e73239ee497bb88fd78903d9a250959
Scott Griffy <scottgriffy@gmail.com>
2015-09-20 20:30:49 -0700

    added a house

--- app.py
+++ app.py
@@ -7,23 +7,24 @@ class IndexHandler(web.RequestHandler):
   def get(self):
     self.render("index.html")
 
-class SocketHandler(websocket.WebSocketHandler):
+class Connection(websocket.WebSocketHandler):
   def check_origin(self, origin):
     return True
-
   def open(self):
+    self.playerName='joe'
     if self not in cl:
       cl.append(self)
-
   def on_close(self):
     if self in cl:
       cl.remove(self)
   def on_message(self, message):
-    self.write_message(u"You said: " + message)
+    self.playerName=message
+    for con in cl:
+      self.write_message(u"playerName: " + con.playerName)
 
 app = web.Application([
   (r'/', IndexHandler),
-  (r'/ws', SocketHandler),
+  (r'/ws', Connection),
   (r'/([a-zA-Z0-9_/]*.png)', web.StaticFileHandler, {'path': './'}),
   (r'/(\w*.ogg)', web.StaticFileHandler, {'path': './'}),
   #(r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
